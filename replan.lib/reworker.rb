714d2be45ccb283b62986c1b97432b9b671dedcc
Saverio Miroddi <saverio.pub2@gmail.com>
2021-11-10 23:29:09 +0100

    Reworker: Implement automatic work end times

--- replan.lib/reworker.rb
+++ replan.lib/reworker.rb
@@ -6,14 +6,48 @@ require_relative 'replan_helper'
 #
 class Reworker
   include ReplanHelper
+  include SharedConstants
 
   # New line is inserted after this.
   #
   INSERTION_POINT_PATTERN = /^( +)(- RSS, email\n)/
 
-  WORK_TASK_PATTERN = /^ *[-~]( \d+:\d+\.)? work (.+)/
+  # Valid reduction/intervals:
+  #
+  # - 666, 66h, 66.66h
+  # - -666, -66h, -66.66h
+  #
+  # That part of the regexp is for the lulz, and in order to easily understand it, after `-?\d+`, read
+  # it from `h)?` backwards.
+  #
+  WORK_TASK_PATTERN = /^( *)[-~]( \d+:\d+\.)? work( -?\d+((\.\d+)?h)?)?$/
 
   ADDED_TASK_TEMPLATE = "- lpimw -t ye '%s' # -c half|off\n"
+
+  # - (start_time, end_time, reduction) and (interval) are two mutually exclusive groups
+  #
+  # - end_time is set when the next entry is found
+  # - reduction is optional
+  #
+  WorkEntry = Struct.new(:original_line, :indentation, :start_time, :end_time, :reduction, :interval) do
+    def self.from(hash)
+      hash.each_with_object(self.new) do |(key, value), entry|
+        entry[key] = value
+      end
+    end
+
+    def to_s
+      if interval
+        interval
+      else
+        "#{start_time}-#{end_time} #{reduction}".rstrip
+      end
+    end
+
+    def inspect
+      original_line.inspect
+    end
+  end
 
   def execute(content)
     current_date = find_first_date(content)
@@ -30,38 +64,75 @@ class Reworker
   end
 
   def extract_work_times(section)
-    all_lines_with_work = section.lines.grep(/\bwork\b/)
+    work_entries = []
 
-    times = all_lines_with_work.map do |work_line|
-      work_line_match = work_line.match(WORK_TASK_PATTERN)
+    # Having the current entry simplifies the logic, rather than inspecting the last work_entries values
+    # to understand if it's current or not.
+    #
+    current_work_entry = nil
 
-      raise "Invalid work line format: #{work_line.strip}" if work_line_match.nil?
+    entries = section.rstrip.lines[1..] # skip header
 
-      raw_time, raw_description = work_line_match[1, 2]
+    entries.each do |line|
+      work_line_match = line.match(WORK_TASK_PATTERN)
 
-      # For the `raw_time` format, see WORK_TASK_PATTERN.
-      #
-      time = raw_time[1...-1] if raw_time
+      if work_line_match
+        raise "Work entries can\'t follow each other! (previous: #{current_work_entry.inspect})" if current_work_entry
 
-      case raw_description
-      when /^-\d+:\d+( -\d+| -\d(\.\d)?+h)?$/
-        # TODO: Check if this check can be generalized into the pattern(s).
-        #
-        raise "Invalid work line format: #{work_line.strip}" if time.nil?
+        indentation, start_time, interval_or_reduction = work_line_match[1..3]
 
-        time + $LAST_MATCH_INFO.to_s
-      when /^(-\d+|-\d\.\d+h) (-\d+:\d+)$/
-        time + $LAST_MATCH_INFO[2] + " " + $LAST_MATCH_INFO[1]
-      when /^\d+(\.\d+)?h$/
-        $LAST_MATCH_INFO.to_s
-      when /^\d+$/
-        $LAST_MATCH_INFO.to_s
+        start_time = start_time&.lstrip&.chomp('.')
+        interval_or_reduction = interval_or_reduction&.lstrip
+
+        case interval_or_reduction
+        when /^-/
+          reduction = interval_or_reduction
+        when //
+          interval = interval_or_reduction
+        end
+
+        if interval
+          # In this case, we don't need a closing entry.
+          #
+          work_entries << WorkEntry.from(
+            original_line: line.strip,
+            indentation:   indentation,
+            interval:      interval
+          )
+        else
+          current_work_entry = WorkEntry.from(
+            original_line: line.strip,
+            indentation:   indentation,
+            start_time:    start_time,
+            reduction:     reduction,
+          )
+        end
       else
-        raise "Unexpected work description found: #{raw_description.inspect}"
+        raise "Invalid work line format: #{line.strip.inspect}" if line.include?("work")
+        next if current_work_entry.nil? # skip lines precending the first work line
+        next if line == TIME_BRACKETS_SEPARATOR
+
+        indentation, start_time = line.match(/^( *)[-.~*]( \d+:\d+\.)?/)[1..]
+
+        start_time = start_time&.lstrip&.chomp('.')
+
+        work_entry_indentation = current_work_entry.indentation
+
+        # Ignore deeper indented lines.
+        #
+        if indentation.size < work_entry_indentation.size
+          raise "Missing closing entry for work entry #{current_work_entry.inspect}"
+        elsif indentation.size == work_entry_indentation.size
+          current_work_entry.end_time = start_time || raise("Subsequent entry has no time! (previous: #{current_work_entry.inspect})")
+          work_entries << current_work_entry
+          current_work_entry = nil
+        end
       end
     end
 
-    times.join(', ')
+    raise "Missing closing entry for work entry #{current_work_entry.inspect}" if current_work_entry
+
+    work_entries.join(', ')
   end
 
   def add_lpim_to_next_day(section, work_times)
