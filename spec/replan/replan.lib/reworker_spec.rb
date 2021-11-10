d9c04d3670d3c0f70d9149362f086a3bbf13671c
Saverio Miroddi <saverio.pub2@gmail.com>
2021-11-10 21:16:08 +0100

    Simplify the main UT in reworker_spec.rb

--- spec/replan/replan.lib/reworker_spec.rb
+++ spec/replan/replan.lib/reworker_spec.rb
@@ -28,17 +28,6 @@ describe Reworker do
     result = subject.execute(content)
 
     expected_result = <<~TEXT
-          MON 07/JUN/2021
-      - 9:00. work -10:00
-      - 10:00. work -13:00 -1.5h
-      - 13:00. work -1.5h -15:00
-        - 15:00. work -16:00 -20
-      - 16:00. work -10 -17:00
-      - 18:00. work -20:00 -1h
-      - 20:00. work 1h
-      - 21:00. work 1.5h
-      ~ 23:00. work 20
-
           TUE 08/JUN/2021
       - foo
         - RSS, email
@@ -47,7 +36,7 @@ describe Reworker do
 
     TEXT
 
-    expect(result).to eql(expected_result)
+    expect(result).to include(expected_result)
   end
 
   context "invalid work lines should raise an error" do
